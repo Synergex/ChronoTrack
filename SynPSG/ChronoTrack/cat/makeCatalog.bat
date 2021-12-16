f31b612ca16b9065eda1a355117a04175e474c29
denisesmelich <56693379+denisesmelich@users.noreply.github.com>
2021-12-16 10:02:28 -0800

    Add files for CodeExchange

---
+++ SynPSG/ChronoTrack/cat/makeCatalog.bat
@@ -0,0 +1,3 @@
+call setenv
+dbcreate -c -p -l catalog_create.log -d CATALOG:
+copy %CATALOG%\chrono_sodbc "%GENESIS_HOME%\"
---
+++ SynPSG/ChronoTrack/cat/updateCatalog.bat
@@ -0,0 +1,2 @@
+call setenv
+dbcreate -x -l catalog_update.log -d CATALOG: -i sodbccnv.ini
